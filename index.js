f965b594e8edb35798075065fecc2bf245926220
Cara R <cara.rosa.02@gmail.com>
2023-10-02 15:03:39 -0500

    added questions for user input

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do users install your application?',
+    },
+    {
+       type: 'input',
+       name: 'usage',
+       message: 'How can users utilize your application?', 
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines to your project?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What are the test instructions to your application?',
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: 'Please choose a license to apply to your application:',
+        choices: ['MIT License', 'Microsoft Public License', 'Academic Free License', 'Artistic License', 'Eclipse Public License'],
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+}
 
 // TODO: Create a function to initialize app
 function init() {}
